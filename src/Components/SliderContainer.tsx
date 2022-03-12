bfb05c31eab61cac9b73f5be8e1065c9e8185bb1
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-13 08:16:48 +0900

    슬라이더 박스 좌우 슬라이딩

--- src/Components/SliderContainer.tsx
+++ src/Components/SliderContainer.tsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 const Slider = styled.div`
     position: relative;
-    top: -180px;
+    top: -120px;
     margin-bottom: 70px;
     height: 250px;
 `
@@ -19,6 +19,8 @@ const Row = styled(motion.div)`
 const Box = styled(motion.div)`
     background-color: white;
     height: 200px;
+    color: red;
+    font-size: 66px;
 `
 const ArrowBtn = styled(motion.div)`
     z-index:10;  // 이걸 안하면 왼쪽 버튼이 아래 깔려서 클릭이 안됨
@@ -42,22 +44,44 @@ const ArrowBtn = styled(motion.div)`
     &:last-child { right: 0; }
 `
 
+const rowVariants = {
+    hidden: (back: boolean) => ({ x: back ? -window.outerWidth + 6 : window.outerWidth - 6 }),
+    visible: { x: 0 },
+    exit: (back: boolean) => ({ x: back ? window.outerWidth - 6 : -window.outerWidth + 6 }),
+}
+
 function SliderContainer() {
+    const [index, setIndex] = useState(0)
+    const [back, setBack] = useState(false)
+    const incIndex = () => {
+        setBack(false)
+        setIndex(prev => prev + 1)
+    }
+    const decIndex = () => {
+        setBack(true)
+        setIndex(prev => prev - 1)
+    }
     return (
         <Slider>
-            <AnimatePresence>
-                <ArrowBtn key="leftBtn">
+            <AnimatePresence
+                initial={false}
+                custom={back}
+            >
+                <ArrowBtn key="leftBtn" onClick={decIndex}>
                     <motion.i key="leftI" className="fas fa-chevron-left"></motion.i>
                 </ArrowBtn>
-                <Row>
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
+                <Row
+                    variants={rowVariants}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    transition={{ type: "tween", duration: 1 }}
+                    key={index}
+                    custom={back}
+                >
+                    {[1, 2, 3, 4, 5, 6].map(i => (<Box key={i}>{i}</Box>))}
                 </Row>
-                <ArrowBtn key="rightBtn">
+                <ArrowBtn key="rightBtn" onClick={incIndex}>
                     <motion.i key="rightI" className="fas fa-chevron-right"></motion.i>
                 </ArrowBtn>
             </AnimatePresence>
