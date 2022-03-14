5bb5f5fe4ec911a1537dbe8206d281197f2e91a4
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-14 11:35:47 +0900

    인트로 제거

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/tv*"><Tv /></Route>
         <Route path="/search"><Search /></Route>
         <Route path="/movies*"><Home /></Route>
-        <Route path="/"><Intro /></Route>
+        <Route path="/"><Home /></Route>
       </Switch>
       <Footer />
     </Router>
